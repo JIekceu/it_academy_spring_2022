af2a8fa9dab35018a30bc4634fd551aa900c088c
Alex <alexmashster@gmail.com>
2022-03-27 23:45:33 +0300

    Add create model functionality

--- lesson/views.py
+++ lesson/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from . import models
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
+from django.contrib.auth.models import User
 from . import models
 from . import forms
 
@@ -45,3 +46,21 @@ def share_material(request, material_id):
     return render(request,
                   'materials/share.html',
                   {'material': material, 'form': form})
+
+
+def create_material(request):
+    if request.method == 'POST':
+        material_form = forms.MaterialForm(request.POST)
+        if material_form.is_valid():
+            new_material = material_form.save(commit=False)
+            new_material.author = User.objects.first()
+            new_material.slug = new_material.title.replace(' ', '-')
+            new_material.save()
+            return render(request,
+                  "materials/detailed_material.html",
+                  {"material": new_material})
+    else:
+        material_form = forms.MaterialForm()
+    return render(request,
+                  'materials/create.html',
+                  {'form': material_form})

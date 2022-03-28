97cf6a924c349f73756cfd255d7481e01b8229f8
Alex <alexmashster@gmail.com>
2022-03-29 02:31:23 +0300

    Add generic login

--- it_academy_spring_2022/settings.py
+++ it_academy_spring_2022/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lesson.apps.LessonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lesson.apps.LessonConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGIN_URL = '/lesson/login/'
+LOGIN_REDIRECT_URL = '/lesson/'
